46cf5bae7067a22b25345a95ec7f54103dd6f4fb
Victoria <36300442+vawosode@users.noreply.github.com>
2019-08-21 10:27:01 +0100

    added js file

---
+++ melacoder.js
@@ -0,0 +1,6 @@
+// Select the carousel you'll need to manipulate and the buttons you'll add events to
+// Also select a card which you'll need for a later step
+const carousel = document.querySelector("[data-target='carousel']");
+const card = carousel.querySelector("[data-target='card']");
+const leftButton = document.querySelector("[data-action='slideLeft']");
+const rightButton = document.querySelector("[data-action='slideRight']");
